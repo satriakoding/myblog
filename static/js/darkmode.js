75523d5999705f5b36fa69f0b63f410cd9e521be
Satria Koding <shatriakoding@gmail.com>
2022-02-23 08:25:05 +0700

    change font

--- static/js/darkmode.js
+++ static/js/darkmode.js
@@ -1,13 +1,13 @@
 var toggle = document.getElementById("dark-mode-toggle");
 var darkTheme = document.getElementById("dark-mode-theme");
 // the default theme is light
-var savedTheme = localStorage.getItem("dark-mode-storage") || "light";setTheme(savedTheme);
+var savedTheme = localStorage.getItem("dark-mode-storage") || "dark";setTheme(savedTheme);
 
 toggle.addEventListener("click", () => {
-    if (toggle.className === "bi bi-moon-stars") {
-        setTheme("dark");
-    } else if (toggle.className === "bi bi-sun") {
+    if (toggle.className === "bi bi-sun") {
         setTheme("light");
+    } else if (toggle.className === "bi bi-moon-stars") {
+        setTheme("dark");
     }
 });
 
